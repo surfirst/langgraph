d9f4c3546e17bce50a331d26ff84b42b3416842c
Fei Yi <yi_fei@hotmail.com>
2024-12-04 12:41:12 +0000

    add more chatbots

---
+++ chatbot-with-human.py
@@ -0,0 +1,89 @@
+from typing import Annotated
+from dotenv import load_dotenv
+from langchain_openai import AzureChatOpenAI, ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.messages import BaseMessage
+from typing_extensions import TypedDict
+from llm_utils import get_llm
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Set up Langchain tracing
+os.environ["LANGCHAIN_TRACING_V2"] = os.getenv("LANGCHAIN_TRACING_V2", "false")
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY", "")
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+
+
+# 使用get_llm()替换原来的llm初始化
+llm = get_llm()
+
+graph_builder = StateGraph(State)
+
+# Get API keys from environment variables
+# api_key = os.getenv('GOOGLE_API_KEY')
+# llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=api_key)
+
+os.environ["TAVILY_API_KEY"] = os.getenv('TAVILY_API_KEY')
+tool = TavilySearchResults(max_results=2)
+tools = [tool]
+
+llm_with_tools = llm.bind_tools(tools)
+
+
+def chatbot(state: State):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+
+graph_builder.add_node("chatbot", chatbot)
+
+tool_node = ToolNode(tools=[tool])
+graph_builder.add_node("tools", tool_node)
+
+graph_builder.add_conditional_edges(
+    "chatbot",
+    tools_condition,
+)
+# Any time a tool is called, we return to the chatbot to decide the next step
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.add_edge(START, "chatbot")
+
+from langgraph.checkpoint.memory import MemorySaver
+
+memory = MemorySaver()
+
+graph = graph_builder.compile(
+    checkpointer=memory,
+    interrupt_before=["tools"])
+
+config = {"configurable": {"thread_id": "1"}}
+
+def stream_graph_updates(user_input: str):
+    for events in graph.stream({"messages": [("human", user_input)]}, config=config):
+        for value in events.values():
+            print("Assistant:", value["messages"][-1].content)
+
+while True:
+    try:
+        user_input = input("User: ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye!")
+            break
+
+        stream_graph_updates(user_input)
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        # fallback if input() is not available
+        user_input = "What do you know about LangGraph?"
+        print("User: " + user_input)
+        stream_graph_updates(user_input)
+        break
